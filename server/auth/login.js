3db62dd88dc62fe3f653af7febce1101c3f3984c
PhantomElite001 <chiemeriamarvelous10@gmail.com>
2026-05-04 21:08:07 +0100

    Fix token generation response in login function

--- server/auth/login.js
+++ server/auth/login.js
@@ -7,24 +7,25 @@ exports.login= async (req,res)=>{
     try {
         const {email,password}=req.body
         if (!email || !password) {
-            return res.status(401).json({message:"missing fields"})
+            throw new Error("missing fields")
         }
         if (!validator.isEmail(email)){
-            return res.status(400).json({message:'invalid email format'})
+            throw new Error("invalid email format")
         }
         if (password.length<8) {
-            return res.status(400).json({message:'password too short'})
+            throw new Error("password too short")
         }
         if(!(await UserModel.checkUser(email))){
-            return res.status(409).json({message="User does not exist"})
+            throw new Error("User does not exist")
         }
         const hashedpass=UserModel.getPassword(email)
         const valid = bcrypt.compare(password,hashedpass)
         if(!valid){
-            return res.status(401).json({message:"wrong password"})
+            throw new Error("wrong password")
         }
         const id = UserModel.getId(email)
-        const token=jwt.sign({email,id},process.env.JWT_SECRET)
+        const token =jwt.sign({email,id},process.env.JWT_SECRET)
+        return token
         } catch (e) {
         
     }
