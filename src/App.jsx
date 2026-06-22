dd1e14f804b906540c834d0dfca8fd7800b00b39
PhantomElite001 <chiemeriamarvelous10@gmail.com>
2026-06-22 11:04:53 +0100

    null

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import react from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Transactions from "./assets/components/Transactions"
 import Home from './pages/Home';
 
 export default function App() {
   return (
     <Router>
       <Routes>
+        // login and registration routes
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
+        // other routes for customer management, transactions, etc. would go here
+        <Route path="/customer/:id" element={<Customer />} />
+        <Route path="/transactions" element={<Transactions />} />
       </Routes>
     </Router>
   );
--- src/assets/components/Receipt.jsx
+++ src/assets/components/Receipt.jsx
@@ -24,4 +24,4 @@ export default function Receipt({tradeDetails}){
 
                 </div>
             </div>
-        )
+        )}
